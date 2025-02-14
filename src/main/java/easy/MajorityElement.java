9984e71486acf2a3ede1d50c6c444b1a531d857c
purnima <purnimamaheshwari021@gmail.com>
2025-02-14 22:58:43 +0000

    Updated submission

---
+++ src/main/java/easy/MajorityElement.java
@@ -0,0 +1,39 @@
+package main.java.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+
+    }
+
+    public static int majorityElement(int[] nums) {
+        int majorityCount = nums.length / 2;
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (countMap.containsKey(nums[i])) {
+                countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
+            } else {
+                countMap.put(nums[i], 1);
+            }
+        }
+
+        final Integer[] checkNumberAndCount = {0, 0};
+
+        countMap.forEach((number, count) -> {
+            if (checkNumberAndCount[1] < count && count >= majorityCount) {
+                checkNumberAndCount[0] = number;
+                checkNumberAndCount[1] = count;
+            }
+        });
+
+        return checkNumberAndCount[0];
+
+    }
+}
