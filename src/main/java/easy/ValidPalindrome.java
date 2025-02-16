89d62694ecf92d0b9e9b05aa6bea577012f04343
purnima <purnimamaheshwari021@gmail.com>
2025-02-16 21:04:07 +0000

    Updated submission

---
+++ src/main/java/easy/ValidPalindrome.java
@@ -0,0 +1,28 @@
+package main.java.easy;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        String s = " ";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        boolean isValidPalindrome = false;
+
+        if (s.isEmpty() || s.length() == 1)
+            return true;
+
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) == s.charAt(j)) {
+                isValidPalindrome = true;
+            } else {
+                return false;
+            }
+        }
+
+        return isValidPalindrome;
+    }
+}
