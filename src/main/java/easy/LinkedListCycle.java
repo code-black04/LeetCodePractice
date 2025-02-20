4d76b5446054181d46bb9590c501d0cf7ddb5605
purnima <purnimamaheshwari021@gmail.com>
2025-02-20 23:40:02 +0000

    Updated submission

--- src/main/java/easy/LinkedListCycle.java
+++ src/main/java/easy/LinkedListCycle.java
@@ -52,14 +52,3 @@ public class LinkedListCycle {
     }
 
 }
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
-    ListNode() {
-    }
-}
---
+++ src/main/java/easy/ListNode.java
@@ -0,0 +1,16 @@
+package main.java.easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    ListNode() {
+    }
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
